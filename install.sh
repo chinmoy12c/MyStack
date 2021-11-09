43d376982cd93fbe7156c3c79ddfe2416238443d
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2021-11-10 01:31:05 +0530

    updated installation script

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ apt-get install docker.io
 apt-get install mysql-server
 mysql -u chinmoy -p mystack < file.sql
 docker build -t tomstack ./webStack/tomcat/
-docker build -t caddyPassGen ./caddy/passwordGenerator/
+docker build -t caddypassgen ./caddy/passwordGenerator/
 docker build -t caddy ./caddy/caddyConnector/
+docker run -it --detach --name=caddyPassGen caddypassgen
