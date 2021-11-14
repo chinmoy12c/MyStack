dce4a13bfe4b803a92a7eb19c4980bf528189577
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2021-11-14 19:50:05 +0530

    tomstack updated

--- myStack/admin/utility/serverConstants.js
+++
@@ -1,9 +0,0 @@
-exports.JWT_SECRET_KEY = 'mysecretkey';
-exports.HASH_SECRET_KEY = 'myhashsecret';
-exports.dbHost = 'localhost';
-exports.dbUser = 'chinmoy';
-exports.dbPassword = 'mysqlpassword';
-exports.database = 'mystack';
-exports.caddyPassGenContainer = 'caddyPassGen';
-exports.scriptsDir = './dockerScripts';
-exports.instanceLimit = 3;
